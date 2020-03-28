d13cd9da5ac79c52451597d24fb05193929483ba
Lukas Simianer <lsimianer@gmail.com>
2020-03-28 17:32:39 -0500

    day two progress

--- storeapp/frontend/.next/static/development/pages/index.js
+++ storeapp/frontend/.next/static/development/pages/index.js
@@ -103,7 +103,7 @@ var Home = function Home(props) {
 
 /***/ }),
 
-/***/ 3:
+/***/ 4:
 /*!******************************!*\
   !*** multi ./pages/index.js ***!
   \******************************/
@@ -128,5 +128,5 @@ module.exports = dll_18682c416555df0bf0b9;
 
 /***/ })
 
-},[[3,"static/runtime/webpack.js"]]]));;
+},[[4,"static/runtime/webpack.js"]]]));;
 //# sourceMappingURL=index.js.map
--- storeapp/frontend/.next/static/runtime/webpack.js
+++ storeapp/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a83900f2a49bbfd22916";
+/******/ 	var hotCurrentHash = "44c63eb3febc79e3261e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
