e986ad4ef9e2de186f12f9e54a44b640fadc552b
Lukas Simianer <lsimianer@gmail.com>
2020-04-01 17:16:19 -0500

    backend running and server started

---
+++ storeapp/backend/src/db.js
@@ -0,0 +1,12 @@
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false // this logs queries in console if true
+});
+
+module.exports = db;
+
+// this file connects remote prisma to db
